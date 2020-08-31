ee188aeae8b7d0f2f26631c56cc1205ee92273e7
ocsarpo <ocsarpo22@gmail.com>
2020-08-31 23:20:42 +0900

    Intro Page

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="hello">
-    Intro
+  <div class="intro">
+    <div class="intro-inner">
+      <h1>Daily<br>Report</h1>
+      <router-link
+        class="button"
+        :to="todayUrl"
+      >
+        START
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   name: 'Intro',
   methods: {
   },
   computed: {
+    todayUrl() {
+      let todayUrl = moment(new Date()).format('YYYY-MM-DD');
+      return `/day/${todayUrl}`;
+    }
   },
   data() {
     return {
